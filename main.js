7260a4938b7a5bb5049e92a211c552555b310677
TORNEDO <rokudokubaradiyabrothers@gmail.com>
2023-04-04 11:16:52 +0530

    who_win_function for cross added  Code

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 
 var h = document.getElementById("won");
+var div = document.getElementById("le");
 
 var t = document.getElementsByClassName("sub-main");
 var b1 = document.getElementById("b1");
@@ -33,11 +34,11 @@ var p = 0 ;
 
 
 
-var res =  ['0','0','0','0','0','0','0','0','0','0'];
+var res =  ['0','0','0','0','0','0','0','0','0'];
 
 
 
-for(let i = 0 ; i < arr.length ; i++){
+for(let i = 0 ; i <= arr.length ; i++){
     let ass = st[i];
     let id = arr[i];
     let c = res[i] ;
@@ -48,8 +49,8 @@ for(let i = 0 ; i < arr.length ; i++){
         
         if(p == 0){
             c = 'o' ;
-            addCircle(id);
             res[i]  = c ;
+            addCircle(id);
             who_win(i,c);
             alert("Player 2 Your Turn");
             p = 1 ;
@@ -100,22 +101,61 @@ function addCross(id){
 }
 
 function who_win(i,play){
+    console.log("WHo_Win Function Called :- ");
     console.log(i,play);
     if(i == 0 || i == 1 || i == 2 ){
         if(res[0] == play && res[1] == play && res[2] == play){
+            // 1ST ROW
+            // le.classList.add(".line");
+            // document.querySelector(".line").style.transform = "translate(8vw, 15vw) rotate(180deg)";
             who_win_Print(play);
         }
+        
+    }else if(i == 0 || i == 3 || i == 6){
+            // LEFT-TO-RIGHT CROSS
+            if(res[0] == play && res[3] == play && res[6] == play){
+                who_win_Print(play);
+            }
+    }else if(i == 0 || i == 4 || i == 8){
+        // 1ST COLUMN 
+        if(res[0] == play && res[4] == play && res[8] == play){
+            who_win_Print(play);
+        }
+    }else if(i == 1 || i == 4 || i == 7){
+        // 2ND COLUMN
+        if(res[1] == play && res[4] == play && res[7] == play){
+            who_win_Print(play); 
+        }
     }
+    else if(i == 3 || i == 4 || i == 5){
+        // 2 ND ROW
+        if(res[3] == play && res[4] == play && res[5] == play){
+            who_win_Print(play); 
+        }
+    } else if(i == 6 || i == 7 || i == 8){
+        // 3 RD ROW
+        console.log("3 rd column");
+        if(res[6] == play && res[7] == play && res[8] == play){
+            console.log("3 rd column");
+            who_win_Print(play); 
+        }
+    }
+
+
+
     return;
 }
 
 
 function who_win_Print(p) {
+    console.log("Printing win player Function Called :-")
+    console.log(p);
     if(p == 'o'){
         h.innerText = "Player 1 Won";
     }else{
         h.innerText = "Player 2 Won";
     }
+    return;
 }
 
 
